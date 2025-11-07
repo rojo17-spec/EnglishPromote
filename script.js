28880039d615efde32e0a710063d1978eb09b2e7
rojo17-spec <juanrojorojo318@gmail.com>
2025-11-07 08:20:06 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -42,3 +42,13 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
+
+// Menú hamburguesa funcional
+const toggleBtn = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.main-nav');
+
+if (toggleBtn && nav) {
+  toggleBtn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+}
